166e135741c56e20a7e11fb813de6b8b555be34f
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-06-30 15:53:19 +0200

    Creazione di div per ogni album + details

--- index.php
+++ index.php
@@ -13,8 +13,17 @@
         <main>
             <div class="container">
 
-                
-
+                <?php 
+                foreach ($albums as $album) { ?>
+                    <div class="album"> 
+                      <h3><?= $album["title"] ?></h3>
+                      <h4><?= $album["author"] ?></h4>
+                      <h5><?= $album["genre"] ?></h5>
+                      <h6><?= $album["year"] ?></h6>
+                    </div>
+                    <?php
+                }
+                ?>
             </div>
         </main>
         
